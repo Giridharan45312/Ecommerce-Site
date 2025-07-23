d265c4330cf316efcc5e67914371a28968a5151b
Giridharan45312 <giritharansivagiritharan@gmail.com>
2025-07-23 14:12:21 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -14,11 +14,11 @@ const Contact = () => {
             <img src={assets.contact_img} className='w-full md:max-w-[480px]' alt="" />
            <div className=' flex flex-col justify-center items-start gap-6'>
                 <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-                <p className='text-gray-500'>Ambattur, Chennai-58</p>
-                <p className='text-gray-500'>Tel: 9940300776<br/> Email: gopilakshmanan72@gmail.com</p>
+                <p className='text-gray-500'>Gummidipoondi, Tiruvallur</p>
+                <p className='text-gray-500'>Tel: 9345721250 <br/> Email: giri22027.it@rmkec.ac.in</p>
                 <p className='font-semibold text-xl text-gray-600'>Carrers With Us</p>
                 <p className='text-gray-500'>Learn more about out teams and job openings.</p>
-                <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'onClick={() => window.location.href = 'https://www.linkedin.com/in/gopikrish01/'}>Explore Jobs</button>
+                <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'onClick={() => window.location.href = 'https://www.linkedin.com/in/giridharan1'}>Explore Jobs</button>
             </div>
         </div>
         <NewsLetterBox />
